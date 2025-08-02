61ec5aacb4aad25ecbe52f8f80b2a062d4a8891f
Sarthakdhekale <sarthakpoonam185@gmail.com>
2025-08-03 00:54:59 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Hello, Crypto Charting 👋</h1>
+      <p>This is your first React + Vite app!</p>
+    </div>
+  );
+}
+
+export default App;
